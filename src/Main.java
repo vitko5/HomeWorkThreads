07a8b30a70fdcf3e39fdbdb7d3d22fd5b1e235c7
vitko5 <cp.advert0@googlemail.com>
2022-07-22 18:14:49 +0300

    push

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,15 @@
-package PACKAGE_NAME;public class Main {
+import java.util.Scanner;
+
+public class Main {
+
+    // Створити потік. Створити клас MyThread наслідувати його від класу Thread. Перевизначити метод run(),
+    // так щоб можна було вивести введену з консолі кількість чисел Фібоначі. Тобто ми вводимо з консолі скільки
+    // ми хочемо бачити чисел Фібоначі і ми їх виводимо з затримкою в 1 секунду.
+   public static void main(String[] args) {
+       Scanner sc = new Scanner(System.in);
+       System.out.print("Enter cout of digits fibonachy: ");
+       MyThread thread = new MyThread(sc.nextInt());
+       thread.start();
+    }
+
 }
--- src/MyThread.java
+++ src/MyThread.java
@@ -1,2 +1,30 @@
-package PACKAGE_NAME;public class MyThread {
+import java.util.ArrayList;
+import java.util.List;
+
+public class MyThread extends Thread {
+    private List<Integer> fibonachi;
+    private int count;
+
+    public MyThread(int count) {
+        this.count = count;
+        this.fibonachi = new ArrayList<>();
+        this.fibonachi.add(1);
+        this.fibonachi.add(1);
+    }
+
+    @Override
+    public void run() {
+        for (int i = 0; i <= count-1; i++) {
+            if (fibonachi.size() <= i) {
+                fibonachi.add(fibonachi.get(i-1) + fibonachi.get(i-2));
+            }
+            System.out.print(" " + fibonachi.get(i));
+            try {
+                sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
 }
